9ffc6dfe0c7079beced3301f4f37a9c47580b466
Generoso Cantu <gcantuw@gmail.com>
2023-12-25 13:38:22 -0600

    auth controller alt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Request, Post, Get, Header, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Header, UseGuards, Req, Res } from '@nestjs/common';
+import { Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../auth/auth.service';
+// import { Request, Response } from 'express';
 // import { CreateItemDto } from './dto/create-item.dto';
 // import { ItemsService } from './items.service';
 // import { Item } from './interfaces/item.interface';
@@ -16,6 +18,19 @@ export class AuthController {
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
+
+  // @UseGuards(AuthGuard('local'))
+  // @Post('/login')
+  // async login(@Req() req, @Res({ passthrough: true }) res: Response) {
+  //   const response = await this.authService.login(req.user);
+
+  //   res.cookie('access_token', response['accessToken'], {
+  //     // httpOnly: true,
+  //     secure: true,
+  //     // sameSite: 'lax',
+  //     expires: new Date(Date.now() + 1 * 24 * 60 * 1000),
+  //   }).send(response.user);
+  // }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('/validate')
